7c10807b30e096372b834c13f3a5602a7e2189b8
Tommy <tommy.reins@gmail.com>
2018-04-07 18:55:18 -0500

    playing around with displaying point clouds

---
+++ pointcloud/build/CMakeFiles/pcl_testing.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pcl_testing.dir/pcl_testing.cpp.o"
+  "pcl_testing.pdb"
+  "pcl_testing"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pcl_testing.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ pointcloud/pcl_testing.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+
+#include <pcl/io/pcd_io.h>
+#include <pcl/io/ply_io.h>
+#include <pcl/point_cloud.h>
+#include <pcl/console/parse.h>
+#include <pcl/common/transforms.h>
+#include <pcl/visualization/pcl_visualizer.h>
+
+int
+  main (int argc, char** argv)
+{
+  pcl::PointCloud<pcl::PointXYZ> cloud;
+
+  // Fill in the cloud data
+  cloud.width    = 5;
+  cloud.height   = 1;
+  cloud.is_dense = false;
+  cloud.points.resize (cloud.width * cloud.height);
+
+  for (size_t i = 0; i < cloud.points.size (); ++i)
+  {
+    cloud.points[i].x = 1024 * rand () / (RAND_MAX + 1.0f);
+    cloud.points[i].y = 1024 * rand () / (RAND_MAX + 1.0f);
+    cloud.points[i].z = 1024 * rand () / (RAND_MAX + 1.0f);
+  }
+
+  pcl::io::savePCDFileASCII ("test_pcd.pcd", cloud);
+  std::cerr << "Saved " << cloud.points.size () << " data points to test_pcd.pcd." << std::endl;
+
+  for (size_t i = 0; i < cloud.points.size (); ++i)
+    std::cerr << "    " << cloud.points[i].x << " " << cloud.points[i].y << " " << cloud.points[i].z << std::endl;
+
+
+  pcl::visualization::PCLVisualizer viewer ("POINT CLOUD TEST");
+  pcl::PointCloud<pcl::PointXYZ>::Ptr source_cloud (new pcl::PointCloud<pcl::PointXYZ> ());
+  pcl::io::loadPCDFile ("test_pcd.pcd", *source_cloud);
+  // Define R,G,B colors for the point cloud
+  pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> source_cloud_color_handler (source_cloud, 255, 255, 255);
+  // We add the point cloud to the viewer and pass the color handler
+  viewer.addPointCloud (source_cloud, source_cloud_color_handler, "original_cloud");
+  //pcl::io::loadPCDFile ("test_pcd.pcd", *newcloud);
+  //pcl::visualization::CloudViewer viewer ("Simple Cloud Viewer");
+  viewer.addCoordinateSystem (1.0, "cloud", 0);
+  viewer.setBackgroundColor(0.05, 0.05, 0.05, 0); // Setting background to a dark grey
+  viewer.setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 2, "original_cloud");
+  //  viewer.showCloud (newcloud);
+  while (!viewer.wasStopped ())
+    {
+      viewer.spinOnce ();
+      
+    }
+  
+  return (0);
+}
